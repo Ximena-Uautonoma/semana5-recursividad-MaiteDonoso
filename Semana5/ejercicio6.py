a2d1fdf9b5afb779b827383e8bd9b2201e82c388
MaiteDonoso <maite.donoso1@cloud.uautonoma.cl>
2026-04-10 01:15:49 +0000

    definitivo

--- Semana5/ejercicio6.py
+++ Semana5/ejercicio6.py
@@ -12,13 +12,20 @@ def total_ventas_ciclo(ventas):
     """
     Retorna el total de ventas usando ciclos.
     """
-    # Escriba aquí su solución
-    pass
+    total = 0
+    for monto in ventas:
+        total += monto
+    return total
+print("El total de las sumas es:", total_ventas_ciclo([150, 200, 50, 300]))
 
 
 def total_ventas_recursivo(ventas):
     """
     Retorna el total de ventas usando recursividad.
     """
-    # Escriba aquí su solución
-    pass
+    if not ventas:
+        return 0
+    valor = ventas [0] #Para iniciar una lista se le asigna un valor 0 para comenzar
+    ventas.pop(0) #Se elimina el primer valor que es 0 para que la lista sea normal
+    return valor + total_ventas_recursivo(ventas)
+print("El total de las sumas es:", total_ventas_recursivo([150, 200, 50, 300]))
